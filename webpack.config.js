078765c5878dd1c68297db4d8ab55ebf50a071e5
Danny Vega <dannyevega@gmail.com>
2018-03-28 16:02:29 -0700

    made app on production mode and hosting on firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -11,18 +12,32 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-    	template: 'app/index.html'
+      template: 'app/index.html'
     })
   ],
-  mode: "development"
+  optimization: {
+    minimize: true
+  }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  )
+}
+
+module.exports = config;
 
 // line 9 & 17-19 needed -- when user refreshes, the browser tries to fetch data from the server at '/popular' for example -- problem is, the router is handling those requests --> adding these lines to webpack.config file tells browser to go back to route dir '/' THEN React will see this and route us to our intended view at '/popular'
